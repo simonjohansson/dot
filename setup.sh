35166c43441dc40e732c7223ebb1b72a29428d51
Simon Johansson <simon@simonjohansson.com>
2013-08-24 18:28:31 +0100

    Add xinitrc

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@ ln -fs $DOT_PATH/.zshrc ~/.zshrc
 
 # Setup the xfce4-terminal
 mkdir -p ~/.config/xfce4/terminal && ln -fs $DOT_PATH/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
+
+ln -fs $DOT_PATH/.xinitrc ~/.xinitrc
